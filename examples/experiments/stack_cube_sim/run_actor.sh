a3a0308c8928102d36c53470b86ff36b6f62bde5
sktii <115221105+sktii@users.noreply.github.com>
2026-01-16 06:14:15 +0000

    delete1

--- examples/experiments/stack_cube_sim/run_actor.sh
+++
@@ -1,6 +0,0 @@
-export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.4 && \
-python ../../train_rlpd.py "$@" \
-    --exp_name=stack_cube_sim \
-    --checkpoint_path=run7\
-    --actor \
